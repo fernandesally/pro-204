d39f38e6f34be3ab688d51833c6ef7264f44bdae
Alaina Fernandes <63796164+fernandesally@users.noreply.github.com>
2023-09-11 17:50:09 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,56 @@
+import socket
+from  threading import Thread
+
+SERVER = None
+PORT = None
+IP_ADDRESS = None
+
+CLIENTS = {}
+
+def acceptConnections():
+    global CLIENTS
+    global SERVER
+
+    while True:
+        player_socket, addr = SERVER.accept()
+        print("hello")
+        player_name=player_socket.recv(1024).decode().strip()
+
+       
+        if(len(CLIENTS.keys())==0):
+          CLIENTS[player_name]={'player_type':'Player1'}
+
+        else:  
+          CLIENTS[player_name]={'player_type':'Player2'} 
+
+        CLIENTS[player_name]["player_socket"] =player_socket 
+        CLIENTS[player_name]["address"]=addr
+        CLIENTS[player_name]["player_name"] =player_name 
+        CLIENTS[player_name]["turn"] =False
+
+        print(f"connection elstablished with {player_name}: {addr}")
+
+def setup():
+    print("\n")
+    print("\t\t\t\t\t\t*** WELCOME TO TAMBOLA HOUSE ***")
+
+
+    global SERVER
+    global PORT
+    global IP_ADDRESS
+
+    IP_ADDRESS = '127.0.0.1'
+    PORT = 6000
+    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    SERVER.bind((IP_ADDRESS, PORT))
+
+    SERVER.listen(10)
+
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
+    print("\n")
+
+
+    
+
+
+setup()
